227f8b76c9935bff9000b9d35456d0ff0e97b2d4
Luciana <l.lopesdealmeida25@gmail.com>
2023-03-17 13:54:19 +0000

    fixing css part 1

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -191,123 +191,43 @@ estimateGroundDryTime(forecast) {
                     </div>
                     {/* {this.state.display ? <Button class={style_iphone.button} clickFunction={this.fetchWeatherData} /> : null} */}
                     {/* Luciana code */}
-                    <div className="backdrop"
-                        style="width: 414px;
-                    height: 736px;">
-                        <div className="group-head"
-                            style="height: 5%;
-                        margin-top: 1%;">
+                    <div class={style.backdrop}>
+                        <div class={style.grouphead}>
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A36?alt=media&token=3f930cc0-d6b5-44f5-8518-ab8e990a466f"
-                                id="icon-menu"
-                                style=" width: 10%;
-                                float: left;
-                                padding-left: 5px;"
+                                class={style.iconmenu}
                             />
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A33?alt=media&token=b3bc6d13-74a4-44c8-98c3-8af245d11c5f"
-                                id="icon-location"
-                                style="width: 3.6%;
-                                position:relative;
-                                left:33%;"
+                                class={style.iconlocation}
                             />
                             <span
-                                id="mile-end-location"
-                                style="color: white;
-                                font-size: 1.5em;
-                                position: relative;
-                                top: 1%;
-                                text-align: right;
-                                padding-left: 35%;"
+                                class={style.currentlocation}
                             >
                                 {this.state.location}
                             </span>
                         </div>
-                        <div className="current-weather"
-                            style="display:block;
-                        background-color:#1CA5C2;
-                        width:100%;">
-                            <p className="_-7"
-                                style=" color: rgba(255, 255, 255, 1);
-                            font-size: 68px;
-                            line-height: 68px;
-                            font-family: Inter, sans-serif;
-                            font-weight: 400;">
+                        <div class={style.currentweather}>
+                            <p class={style.temperaturenow}>
                                 {Math.round(this.state.temperature)}
                                 °</p>
-                            <p className="sunny"
-                                style="color: rgba(255, 255, 255, 1);
-                                font-size: 36px;
-                                line-height: 36px;
-                                font-family: Inter, sans-serif;
-                                font-weight: 400;
-                                top:10px;
-                                left:20px;">
+                            <p class={style.weathertype}>
                                 Sunny</p>
-                            <p className="rain-expected-later-tod"
-                                style="
-                            color: rgba(255, 255, 255, 1);
-                            font-size: 16px;
-                            line-height: 16px;
-                            font-family: Inter, sans-serif;
-                            font-weight: 400;
-                            opacity: 0.8;
-                            display:block;">
+                            <p class={style.stateweather}>
                                 {this.state.description}
                             </p>
                         </div>
-                        <div className="walk-recommendation"
-                            style="float: right;
-                            background-color: #1CA5C2;
-                            width:40%;
-                            height: 80px;
-                            margin-top:15px;">
-                            <p className="best-times-for-walking"
-                                style=" display: flex;
-                                flex-direction: column;
-                                justify-content: center;
-                                color: rgba(255, 255, 255, 1);
-                                font-size: 16px;
-                                line-height: 16px;
-                                font-family: Inter, sans-serif;
-                                font-weight: 400;
-                                text-align: center;
-                                margin-top:3%">Best time for walking:</p>
-                            <p className="_-1pm-5pm"
-                                style=" display: flex;
-                                flex-direction: column;
-                                justify-content: center;
-                                color: rgba(255, 255, 255, 1);
-                                font-size: 20px;
-                                line-height: 20px;
-                                font-family: Inter, sans-serif;
-                                font-weight: 400;
-                                text-align: center;
-                                margin-top:5%">
+                        <div class={style.walkrecommendation}>
+                                <p class={style.walktext}>Best time for walking:</p>
+                            <p class={style.besttime}>
 									{this.state.bestTime.time}
                                 </p>
                         </div>
-                        <div className="weather-later"
-                            style="float: right;
-                        background-color: #1CA5C2;
-                        width:40%;
-                        height:20%;
-                        margin-top:5%;
-                        margin-left:50%">
+                        <div class={style.weatherlater}>
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A166?alt=media&token=171d43cb-994d-494d-9d14-192738004dbe"
-                                className="icon-raindrops"
-                                style="width: 25%;
-                                padding-top: 20px;
-                                padding-left: 5px;
-                                float:left;"
-                            />
-                            <p className="ground-expected-to-be-we"
-                                style="margin-left:5%;
-                                display: block;
-                                margin-top: 20px;
-                                color:white;
-                                font-size:15px;">
+                                class={style.iconweatherlater}/>
+                            <p class={style.groundstate}>
                                 {groundDryTime === 0 ? (
 								<p>The ground is currently dry</p>
 							) : (
@@ -315,55 +235,23 @@ estimateGroundDryTime(forecast) {
 							)}
                             </p>
                         </div>
-                        <div className="graphic-dog"/>
+                        <div class={style.graphicpet}/>
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A88?alt=media&token=295334cf-2623-403b-866b-c88285a48535"
-                                className="icon-sun"
-                                style="display: inline;
-                                bottom: 0%;
-                                top: 5%;
-                                position: relative;
-                                left:15%"
-                            />
+                                class={style.iconsun}/>
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A43?alt=media&token=10c7b2d3-f915-4f98-a55f-0d6980834db7"
-                                className="icon-cloud"
-                                style="display: inline;
-                                bottom: 0%;
-                                position: relative;"
-                            />
+                                class={style.iconcloud}/>
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A44?alt=media&token=d66ede89-0eb8-4f83-ba0d-ee515f159055"
-                                className="icon-cloud-1"
-                                style="display: inline;
-                                bottom: 37%;
-                                position: relative;"
+                                class={style.iconcloud1}
                             />
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A27?alt=media&token=cb4de56b-46e7-40a6-a85c-471f1442460f"
-                                className="dog-logo-illustration-free-vector-1"
-                                style="display: inline;
-                                bottom: 20%;
-                                position: relative;
-                                right: 20%;"
-                            />
+                                class={style.petdrawing}/>
                         </div>
-                    <div className="hourly-weather"
-                            style="display: block;
-                            color: white;
-                            position: relative;
-                            bottom: 35%;
-                            width: 100%;
-                            text-align: center;
-                            // text-align-last: justify;
-                            margin-left: 5%;
-                            margin-right: 5%;
-                            margin-bottom:5%;">
-                        <p className="_-46710101210988"
-                            style="display: block;
-                            text-align-last: justify;
-                            font-size:14px;
-                            width:90%;"
+                    <div class={style.hourlyweather}>
+                        <p class={style.timeweather}
                             >
                         <div>
                         {next5HourForecast.map((item, index) => (
@@ -373,33 +261,24 @@ estimateGroundDryTime(forecast) {
                             </p>
                             
 
-                            <div className="hourly-weather-icons"
-                            style="height:20%;
-                            display:block;
-                            padding-bottom:10%;">
-                            <div style={{ width: '90%', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                            <div class={style.hourlyweathericons}>
+                            <div class={style.hourlydiv}>
                             {next5HourForecast.map((item, index) => (
-                                <div key={index} style={{ display: 'inline-flex', alignItems: 'center', marginRight: '10px' }}>
+                                <div key={index} class={style.hourdiv2}>
                                 {item.condition.includes('sunny') && (
                                     <img
                                     src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A94?alt=media&token=bb7e90a0-73ed-4fc4-afe3-5088bbd86caf"
-                                    className="icon-sun-1"
-                                    style={{ width: '25px', height: '25px', marginRight: '5px' }}
-                                    />
+                                    class={style.hourweathericons}/>
                                 )}
                                 {item.condition.includes('rain') && (
                                     <img
                                     src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A168?alt=media&token=46eabaa4-87c5-4151-aea4-cc84051c75e6"
-                                    className="icon-night-alt-rain"
-                                    style={{ width: '25px', height: '25px', marginRight: '5px' }}
-                                    />
+                                    class={style.hourweathericons}/>
                                 )}
                                 {item.condition.includes('clouds') && (
                                     <img
                                     src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A172?alt=media&token=44d3d1eb-25a6-44bb-945b-bcea1585f56e"
-                                    className="icon-night-alt-partly-cloudy"
-                                    style={{ width: '25px', height: '25px', marginRight: '5px' }}
-                                    />
+                                    class={style.hourweathericons}/>
                                 )}
                                 <span>{(index < next5HourForecast.length - 1) ? ' ' : ''}</span>
                                 </div>
@@ -410,17 +289,8 @@ estimateGroundDryTime(forecast) {
                             </div>
                             <img
                                 src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-1%3A48?alt=media&token=2f1d3d24-b8a4-4074-90c2-e7dab0915f21"
-                                className="line-1" style="margin-top: 5%;
-                                width: 90%;
-                                right: 5%;
-                                bottom: 3%;
-                                position: relative;"
-                            />
-                            <p className="_-12345678910"
-                                style="display: block;
-                                text-align-last: justify;
-                                font-size:15px;
-                                width:90%;"
+                                class={style.hourlyline}/>
+                            <p class={style.hours}
                                 >
                                 {forecast.map((item, index) => (
 									<span key={index}>{item.time} </span>
@@ -432,53 +302,20 @@ estimateGroundDryTime(forecast) {
 
                         </div>
 
-                        <div className="wind-humid-hot"
-                            style="display: block;
-                            width: 100%;
-                            height: 10%;
-                            position: relative;
-                            bottom: 35%;
-                            background-color: #1ca5c2;
-                            color:white;"
-                            >
-                            <div className="wind-humid-hot-icons"
-                                style="display:block;
-                                width:100%;
-                                height: 50px;
-                                margin-left:10%;
-                                padding-top: 1%;
-                                margin-bottom: 2%;">
+                        <div class={style.windhumidcloud}>
+                            <div class={style.windhumidcloudicons}>
                                 <img
                                     src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-13%3A11?alt=media&token=05527e3f-a85f-4a04-982a-ea2636df350c"
-                                    className="icon-strong-wind"
-                                    style="width:10%;
-                                    position:relative;
-                                    display: inline;
-                                    "
-                                />
+                                    class={style.iconswind}/>
                                 <img
                                     src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-13%3A14?alt=media&token=36972f3e-4297-49be-a97b-c02cd9893057"
-                                    className="icon-humidity"
-                                    style="width:10%;
-                                    position:relative;
-                                    display: inline;
-                                    margin-left:25%;
-                                    margin-right:25%"
-                                />
+                                    class={style.iconhumidity}/>
                                 <img
                                     src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/k6fpuppe53f-13%3A23?alt=media&token=6aaf4db8-9a52-4518-99b3-3e40f27d3929"
-                                    className="icon-hot"
-                                    style="width:10%;
-                                    position:relative;
-                                    display: inline;"
-                                />
+                                    class={style.iconhot}/>
                                 
                             </div>
-                            <p className="windspeed" 
-                            style="font-size: 15px;
-                            color: rgba(255,255,255,0.7);
-                            padding-left: 5%;"
-                            >Windspeed</p>
+                            <p class={style.windspeed}>Windspeed</p>
                             <p className="humidity"
                             style="font-size: 15px;
                             color: rgba(255,255,255,0.7);
