31b120e2cadce31112ed0e83c074591be2a424a8
Luciana <l.lopesdealmeida25@gmail.com>
2023-03-22 13:09:16 +0000

    hamburger menu changes

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import { h, Component } from 'preact';
 // import required Components from 'components/'
 import Iphone from './iphone';
 import Ipad from './ipad';
-import style from '../iphone/style.less'
+import style from './iphone/style.less'
 
 export default class App extends Component {
 //var App = React.createClass({
